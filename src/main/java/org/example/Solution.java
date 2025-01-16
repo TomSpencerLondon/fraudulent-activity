da77791af1a3f2761d2dc3e9a6a9f0d4200dcb8f
tom.spencer <tom.spencer@defra.onmicrosoft.com>
2025-01-16 06:50:47 +0000

    add code

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,34 @@
+package org.example;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        ClassLoader classLoader = Solution.class.getClassLoader();
+        File inputFile = new File(classLoader.getResource("large/input.txt").getFile());
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/main/resources/large/output.txt"));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> expenditure = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.activityNotifications(expenditure, d);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
